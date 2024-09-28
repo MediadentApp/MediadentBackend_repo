23dd594030583880f53476712906c267e9bf05a6
VIN <v3p51435@gmail.com>
2024-09-28 12:29:19 +0530

    Merge backend_deployment into main

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const initUserFormat = require('@src/seeders/userFormatSeeds');
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 mongoose.connect(DB).then(() => console.log('DB connected'));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
   // initUserFormat();

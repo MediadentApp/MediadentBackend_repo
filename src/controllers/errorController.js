15fb2366b7ca1bc71e0fe390e5b582fc0dc2df5d
VIN <v3p51435@gmail.com>
2024-09-28 12:58:52 +0530

    Updated production error response

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -59,15 +59,13 @@ const sendErrorProd = (err, res) => {
 };
 
 module.exports = (err, req, res, next) => {
-  // console.log(err.stack);
-
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'Internal Server Error';
 
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    let error = { ...err };
+    let error = err;
 
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);

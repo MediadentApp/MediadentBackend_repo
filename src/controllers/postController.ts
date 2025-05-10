0ad7e822bc4ec83be953cf0c83ed924e3fe3bb6e
VIN <vinay.poojary@rthetapi.com>
2025-05-10 11:42:47 +0530

    Minor changes related to lambda func

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -1,25 +1,46 @@
-import responseMessages from "#src/config/constants/responseMessages.js";
-import { uploadToS3 } from "#src/libs/s3.js";
-import { AppRequestBody } from "#src/types/api.request.js";
-import { AppResponse } from "#src/types/api.response.js";
-import { PostBody } from "#src/types/request.post.js";
-import ApiResponse from "#src/utils/ApiResponse.js";
-import catchAsync from "#src/utils/catchAsync.js";
-import { NextFunction } from "express";
+import responseMessages from '#src/config/constants/responseMessages.js';
+import { uploadToS3 } from '#src/libs/s3.js';
+import { AppRequestBody } from '#src/types/api.request.js';
+import { AppResponse } from '#src/types/api.response.js';
+import { PostBody } from '#src/types/request.post.js';
+import ApiResponse from '#src/utils/ApiResponse.js';
+import catchAsync from '#src/utils/catchAsync.js';
+import axios from 'axios';
+import { NextFunction } from 'express';
 
 export const posts = catchAsync(async (req: AppRequestBody<PostBody>, res: AppResponse, next: NextFunction) => {
-    console.log('posts', req.body)
-    console.log('req.files', req.files)
+  console.log('posts', req.body);
+  console.log('req.files', req.files);
+  console.log('Request Content-Type:', req.headers['content-type']);
+  console.log('Files received:', req.files?.length);
 
-    const files = req.files as Array<Express.Multer.File>
+  const files = req.files as Array<Express.Multer.File>;
 
-    let data
-    if (files && files.length) {
-        const s3Result = await Promise.all(files.map(file => uploadToS3(file)))
-        data = {
-            files: s3Result
-        }
-    }
+  if (!files || files.length === 0) {
+    return ApiResponse(res, 400, 'no images');
+  }
 
-    ApiResponse(res, 200, responseMessages.GENERAL.SUCCESS, data);
-})
+  const s3UploadUrl = process.env.AWS_IMAGE_UPLOAD_LAMBDA;
+  if (!s3UploadUrl) {
+    console.error('AWS_IMAGE_UPLOAD_LAMBDA is not defined');
+    return ApiResponse(res, 500, responseMessages.GENERAL.SERVER_ERROR);
+  }
+
+  const convertedFiles = files.map(file => ({
+    fileBase64: file.buffer.toString('base64'),
+    fileName: file.originalname,
+    mimeType: file.mimetype,
+    // Optional config per file
+    config: {
+      format: 'jpeg',
+      quality: 70,
+    },
+  }));
+
+  const response = await axios.post(s3UploadUrl, {
+    username: req.user.username,
+    files: convertedFiles,
+  });
+
+  ApiResponse(res, 200, responseMessages.GENERAL.SUCCESS, response.data);
+});
--- src/middlewares/multerPosts.ts
+++ src/middlewares/multerPosts.ts
@@ -1,32 +1,32 @@
-import appConfig from "#src/config/appConfig.js";
-import { ErrorCodes } from "#src/config/constants/errorCodes.js";
-import responseMessages from "#src/config/constants/responseMessages.js";
-import ApiError from "#src/utils/ApiError.js";
-import { Request } from "express";
-import multer from "multer";
-import path from "path";
+import appConfig from '#src/config/appConfig.js';
+import { ErrorCodes } from '#src/config/constants/errorCodes.js';
+import responseMessages from '#src/config/constants/responseMessages.js';
+import ApiError from '#src/utils/ApiError.js';
+import { Request } from 'express';
+import multer from 'multer';
+import path from 'path';
 
 // Allowed file types
-const allowedTypes = appConfig.app.post.allowedPostsImageType
+const allowedTypes = appConfig.app.post.allowedPostsImageType;
 
 const fileFilter = (req: Request, file: Express.Multer.File, cb: any) => {
-    console.log('file', file)
-    const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
-    const mimetype = allowedTypes.test(file.mimetype);
-    if (extname && mimetype) {
-        return cb(null, true);
-    }
-    cb(new ApiError(responseMessages.DATA.INVALID_IMAGE, 400, ErrorCodes.DATA.POST_IMAGE_TYPE_INVALID))
+  console.log('file', file);
+  const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = allowedTypes.test(file.mimetype);
+  if (extname && mimetype) {
+    return cb(null, true);
+  }
+  cb(new ApiError(responseMessages.DATA.INVALID_IMAGE, 400, ErrorCodes.DATA.POST_IMAGE_TYPE_INVALID));
 };
 
 // Multer route
 const upload = multer({
-    storage: multer.memoryStorage(), // ← Store files in memory
-    limits: {
-        fileSize: appConfig.app.post.postsMaxImageSize
-    },
-    fileFilter
-})
+  storage: multer.memoryStorage(), // ← Store files in memory
+  limits: {
+    fileSize: appConfig.app.post.postsMaxImageSize,
+  },
+  fileFilter,
+});
 
-const postUpload = upload.array('data', appConfig.app.post.allowedPostImagesPerPost)
-export default postUpload
+const postUpload = upload.array('files', appConfig.app.post.allowedPostImagesPerPost);
+export default postUpload;

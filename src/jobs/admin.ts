167cb006adcaf5b39032e57fd901c7c4394d9cca
vin <v3p51435@gmail.com>
2026-01-26 11:13:46 +0530

    fixed bullMqAdapter import

--- src/jobs/admin.ts
+++ src/jobs/admin.ts
@@ -3,7 +3,7 @@ import { postViewCleanupQueue } from '#src/jobs/queues/postView.queue.js';
 import { testQueue } from '#src/jobs/queues/test.queue.js';
 import { updateTaggedPostsQueue } from '#src/jobs/queues/updateTaggedPosts.queue.js';
 import { createBullBoard } from '@bull-board/api';
-import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
+import { BullMQAdapter } from '@bull-board/api/bullMQAdapter.js';
 import { ExpressAdapter } from '@bull-board/express';
 
 const serverAdapter = new ExpressAdapter();
